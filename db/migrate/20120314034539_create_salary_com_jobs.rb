c629546f0586c11e99a67d6871bfe41348281dde
Narendra Chennamsetty <narench@gmail.com>
2012-03-24 02:58:25 -0400

    fixing salary table migration heroku

--- db/migrate/20120314034539_create_salary_com_jobs.rb
+++ db/migrate/20120314034539_create_salary_com_jobs.rb
@@ -1,10 +1,14 @@
 class CreateSalaryComJobs < ActiveRecord::Migration
-  def change
+  def up
     create_table :salary_com_jobs do |t|
       t.string :title
       t.string :code
       t.string :bls_job_code
       t.timestamps
     end
+  end
+  
+  def down
+    drop_table :salary_com_jobs
   end
 end
