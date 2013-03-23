4674597422244967e5e525392aa5b6449a1928d5
isabellaly <yi.isabella@aol.com>
2013-03-23 13:00:19 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ACoolApp::Application.config.secret_token = 'dc71004f926767e3314789915f524d5b154ed8458329c66e6e1b3ed7b64f9b72daaf97d83c00e66f28d292f5dd6ab9e0a69bbba231184944f35eddc6e2d7215c'
